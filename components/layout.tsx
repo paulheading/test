6feefa4a403419c786bfaa8958b98787186dd0e7
Paul Heading <paulheading@me.com>
2022-02-15 22:14:30 +0000

    switch off

--- components/layout.tsx
+++ components/layout.tsx
@@ -6,19 +6,19 @@ import { env } from 'scripts/variables'
 
 export const Layout:React.FC = ({ children }) => {
 
-  if (!env.is('test')) {
-    getTrello();
-    setCount();
-    setHero();
-    getSpotify();
-    getMedium();
-    getNpm();
-    getGem();
-  }
+  // if (!env.is('test')) {
+  //   getTrello();
+  //   setCount();
+  //   setHero();
+  //   getSpotify();
+  //   getMedium();
+  //   getNpm();
+  //   getGem();
+  // }
 
-  if (env.is('local')) {
-    console.log("state: ", useSelector(state => state))
-  }
+  // if (env.is('local')) {
+  //   console.log("state: ", useSelector(state => state))
+  // }
   
   return (
     <div className={styles.container}>
